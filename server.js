e5480c98d52481427d81c31f3c0d708f3994bc72
jyanev01 <jordanyanev@gmail.com>
2021-11-11 16:29:14 -0500

    add and delete candidates

--- server.js
+++ server.js
@@ -31,6 +31,34 @@ db.query(`SELECT * FROM candidates`, (err, rows) => {
     console.log(rows);
 });
 
+// GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id= 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+
+// // DELETE a candidate
+// db.query(`DELETE FROM candidate WHERE id =?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a candidate
+const sql =`INSERT INTO candidates (id, first_name, last_name, industry_connected)
+            VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
