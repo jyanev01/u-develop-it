578f08412e810070325ec78d40e1c7a1973f34bf
jyanev01 <jordanyanev@gmail.com>
2021-11-11 16:07:18 -0500

    updated"

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +8,19 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'Richmond2!',
+        database: 'election'
+    },
+    console.log('Connected to the election databse.')
+);
 
 // Default response for any other requst (Not Found)
 app.use((req, res) => {
