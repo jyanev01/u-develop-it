080579ddef84964b4fcb4852d0e6d5dae5df3430
jyanev01 <jordanyanev@gmail.com>
2021-11-15 06:05:33 -0500

    adding CRUD

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -22,30 +23,22 @@ const db = mysql.createConnection(
     console.log('Connected to the election databse.')
 );
 
-// Default response for any other requst (Not Found)
-// app.use((req, res) => {
-//     res.status(404).end();
-// });
-
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// });
 
 // GET all candidates
-// app.get('/api/candidates', (req, res) => {
-//     const sql =`SELECT * FROM candidates`;
+app.get('/api/candidates', (req, res) => {
+    const sql =`SELECT * FROM candidates`;
 
-//     db.query(sql, (err, rows) => {
-//         if (err) {
-//             res.status(500).json({ error: err.message });
-//             return;
-//         }
-//         res.json({
-//             message: 'success',
-//             data: rows
-//         });
-//     });
-// });
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
 
 
 
@@ -90,23 +83,34 @@ app.delete('/api/candidate/:id', (req, res) => {
 });
 
 // Create a candidate
-const sql =`INSERT INTO candidates (id, first_name, last_name, industry_connected)
-            VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-
-db.query(sql, params, (err, result) => {
-    if (err) {
-        console.log(err);
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(
+        body,
+        'first_name',
+        'last_name',
+        'industry_connected'
+    );
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
     }
-    console.log(result);
+    const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+            VALUES (?,?,?,?)`;
+    const params = [body.first_name,  body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            rs.status(400).json({ error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
 });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'Hello World'
-//     });
-// });
