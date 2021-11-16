fc015abca53730dbfa2d782f1aa34027bd8f8552
jyanev01 <jordanyanev@gmail.com>
2021-11-16 07:24:34 -0500

    updates

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../../db/connections');
 const inputCheck = require('../../utils/inputCheck');
 
-// GET ALL voters
+// GET ALL voters alphabetized by last name
 router.get('/voters', (req, res) => {
     const sql = `SELECT * FROM voters ORDER BY last_name`;
 
@@ -36,6 +36,7 @@ router.get('/voter/:id', (req, res) => {
     });
 });
 
+// Create a voter
 router.post('/voter', ({ body }, res) => {
     // Data validation 
     const errors = inputCheck(body, 'first_name', 'last_name', 'email');
@@ -61,7 +62,7 @@ router.post('/voter', ({ body }, res) => {
     });
 });
 
-
+// Update a voter's email
 router.put('/voter/:id', (req, res) => {
     // Data validation
     const errors = inputCheck(req.body, 'email');
@@ -90,6 +91,7 @@ router.put('/voter/:id', (req, res) => {
     });
 });
 
+// Delete a voter
 router.delete('/voter/:id', (req, res) => {
     const sql = `DELETE FROM voters WHERE id = ?`;
 
